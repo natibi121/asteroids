cd05914e88fac0e0bf899298f3a8fc7ca50c577d
natibi121 <natibi279@gmail.com>
2026-03-03 17:51:49 +0100

    B version

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,8 +19,8 @@ def main():
                 return
         screen.fill("black") 
         pygame.display.flip()
-        
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
